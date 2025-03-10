b3b2ca67f82ec67e25cecf74780734c44b1840a5
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2025-03-10 16:37:29 -0400

    Add projects

--- fy25/__init__.py
+++ fy25/__init__.py
@@ -1,5 +1,11 @@
-from sanic import Blueprint, Request, text
+import json
+from aiohttp import FormData
+from sanic import Blueprint, HTTPResponse, Request, text
+from sanic_ext import render
 
+from utils import fetch_file, fetch_json
+
+yr = "25new"
 bp = Blueprint("fy25", url_prefix="/25")
 
 
@@ -8,9 +14,27 @@ async def index(request: Request):
     return text("Hello World!")
 
 
-@bp.get("/project/:project_id")
+@bp.get("/project/<project_id:str>")
 async def project(request: Request, project_id):
-    return text(f"Project {project_id}")
+    result = await fetch_json(f"https://apps.dvrpc.org/ords/workprogram{yr}/workprogram/projects?proid={project_id}")
+    if not result:
+        return text(f"Project {project_id} database request failed")
+    if result["items"] == []:
+        return text(f"Project {project_id} not found")
+    csstemplate = await render("fy26/styles.css", context={"pageno": 1})
+    resultcss = result.copy()
+    resultcss["css"] = csstemplate.body.decode()
+    pretemplate = await render("fy26/pre.html", context=resultcss)
+    template = ""
+    for item in result["items"]:
+        item["funding_details"] = json.loads(
+            item["funding_details"])
+        item["lrpimages"] = json.loads(
+            item["lrpimages"]) if item["lrpimages"] else []
+        template += (await render("fy26/project.html", context=item)).body.decode()
+    posttemplate = await render("fy26/post.html", context=result)
+    submission = await fetch_file("https://cloud.dvrpc.org/api/pdf_gen/pdf", method="POST", data=FormData({"html": pretemplate.body.decode() + template + posttemplate.body.decode(), "css": csstemplate.body.decode()}))
+    return HTTPResponse(body=submission, content_type="application/pdf")
 
 
 @bp.get("/chapter/:chapter_id")
--- fy26/__init__.py
+++ fy26/__init__.py
@@ -17,17 +17,23 @@ async def index(request: Request):
 @bp.get("/project/<project_id:str>")
 async def project(request: Request, project_id):
     result = await fetch_json(f"https://apps.dvrpc.org/ords/workprogram{yr}/workprogram/projects?proid={project_id}")
-    result["items"][0]["funding_details"] = json.loads(
-        result["items"][0]["funding_details"])
-    result["items"][0]["lrpimages"] = json.loads(
-        result["items"][0]["lrpimages"])
-    csstemplate = await render("fy26/styles.css", context=result)
+    if not result:
+        return text(f"Project {project_id} database request failed")
+    if result["items"] == []:
+        return text(f"Project {project_id} not found")
+    csstemplate = await render("fy26/styles.css", context={"pageno": 1})
     resultcss = result.copy()
     resultcss["css"] = csstemplate.body.decode()
     pretemplate = await render("fy26/pre.html", context=resultcss)
-    template = await render("fy26/project.html", context=result)
+    template = ""
+    for item in result["items"]:
+        item["funding_details"] = json.loads(
+            item["funding_details"])
+        item["lrpimages"] = json.loads(
+            item["lrpimages"]) if item["lrpimages"] else []
+        template += (await render("fy26/project.html", context=item)).body.decode()
     posttemplate = await render("fy26/post.html", context=result)
-    submission = await fetch_file("https://cloud.dvrpc.org/api/pdf_gen/pdf", method="POST", data=FormData({"html": pretemplate.body.decode() + template.body.decode() + posttemplate.body.decode(), "css": csstemplate.body.decode()}))
+    submission = await fetch_file("https://cloud.dvrpc.org/api/pdf_gen/pdf", method="POST", data=FormData({"html": pretemplate.body.decode() + template + posttemplate.body.decode(), "css": csstemplate.body.decode()}))
     return HTTPResponse(body=submission, content_type="application/pdf")
 
 
