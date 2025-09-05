ba4deeb7dbf0d8164ff4f31eb8b44222d3fdce8f
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2025-09-05 22:25:52 +0000

    Change Completion label to Spent

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import aiohttp
 
 
-async def fetch_json(url):
+async def fetch_json(url, **options):
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
+        async with session.get(url, allow_redirects=True, **options) as response:
             if response.status == 200:
                 return await response.json()
             else:
